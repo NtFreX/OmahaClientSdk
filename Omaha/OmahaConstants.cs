23676582bcdd8b4c00c169a9b2ee0ca11c2d9870
Frederik Traber <frederik.traber@voicepoint.ch>
2017-07-24 18:58:41 +0200

    change defaults

--- Omaha/OmahaConstants.cs
+++ Omaha/OmahaConstants.cs
@@ -4,7 +4,7 @@ namespace Omaha
 {
     public static class OmahaConstants
     {
-        public static readonly string CompanyName = "Voicepoint";
+        public static readonly string CompanyName = "MyCompany";
         public static readonly string AppName = "Omaha";
         public static readonly LogLevel LogLevel = LogLevel.Trace;
     }
